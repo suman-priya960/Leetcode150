18be1ebd45e1178c47741a3fe04c191bf836df9a
suman-priya960 <priya34suman@gmail.com>
2026-03-23 21:41:00 +0530

    solved array problems

--- Arrays/rotatearray.java
+++ Arrays/rotatearray.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 public class rotatearray {
     public void rotate(int[] nums, int k) {
         int n=nums.length;
@@ -18,24 +18,3 @@ public class rotatearray {
         }
     }
 }
-=======
-public class rotatearray {
-    public void rotate(int[] nums, int k) {
-        int n=nums.length;
-        k=k%n;
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
-
-    }
-    private void reverse(int[]nums,int i,int j){
-        while(i<j){
-            int temp=nums[i];
-            nums[i]=nums[j];
-            nums[j]=temp;
-            i++;
-            j--;
-        }
-    }
-}
->>>>>>> 1122a47 (solved rotate array and longestof longest substring.java)
